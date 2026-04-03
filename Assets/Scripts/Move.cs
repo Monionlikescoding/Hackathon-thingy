950876693c3c8aef000d14d0b460c2c08c55f19a
Monionlikescoding <2027patelmanya@gmail.com>
2026-04-03 18:05:59 -0400

    yep

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    InputAction moveAction;
+    public Rigidbody2D playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        moveValue.y=0; // you can only move in the x-direction
+
+        playerRb.AddForce(moveValue * speed);
+
+        Vector2 vel = playerRb.linearVelocity;
+        vel.x = Mathf.Clamp(vel.x, -speed/12, speed/12); // clamping x-velocity to speed
+        playerRb.linearVelocity = vel;
+    }
+}
