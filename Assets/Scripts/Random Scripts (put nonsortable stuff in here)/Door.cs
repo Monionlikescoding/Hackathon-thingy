d0f87cf9e3ef27c7a6adb896c89a9d53f006dd4b
Monionlikescoding <2027patelmanya@gmail.com>
2026-04-08 18:58:35 -0400

    yerfsd
    yedggd

---
+++ Assets/Scripts/Random Scripts (put nonsortable stuff in here)/Door.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Use tags to identify what entered the zone
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("open");
+        }
+    }
+}
