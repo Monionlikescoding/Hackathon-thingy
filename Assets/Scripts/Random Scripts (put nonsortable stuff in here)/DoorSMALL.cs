91afde4ec2789ecdb712d6e62ea3a40823752995
Monionlikescoding <2027patelmanya@gmail.com>
2026-04-09 16:50:32 -0400

    fixed doors

---
+++ Assets/Scripts/Random Scripts (put nonsortable stuff in here)/DoorSMALL.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class DoorSMALL : MonoBehaviour
+{
+    Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Use tags to identify what entered the zone
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("openSMALL",true);
+            
+        }
+        
+    }
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.CompareTag("Player"))
+        {
+            anim.SetBool("openSMALL",false);
+        }
+	}
+}
