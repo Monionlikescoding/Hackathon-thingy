70f4bb0e5b66ff7e8f23d8a6af810788c9254217
Monionlikescoding <2027patelmanya@gmail.com>
2026-04-10 17:55:52 -0400

    yep
    yep

--- Assets/Scripts/Background main scripts/GameManager.cs
+++ Assets/Scripts/Background main scripts/GameManager.cs
@@ -5,6 +5,9 @@ public class GameManager : MonoBehaviour
 {
     //public GameObject[] doors;
     public Dictionary<string, string[]> abnoInfo; // Literally just a map but in c#
+    public GameObject[] rooms;
+    public GameObject player;
+    public Camera mainCam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +16,9 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        Vector3 roomPos = rooms[player.GetComponent<Move>().RoomId].transform.position;
+        roomPos.z -= 10;
+        mainCam.transform.position = roomPos;
     }
 }
