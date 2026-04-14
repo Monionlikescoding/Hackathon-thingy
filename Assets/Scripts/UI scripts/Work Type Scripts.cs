15301481f03fb62a9a70ad7998f857c1056bf54e
Monionlikescoding <2027patelmanya@gmail.com>
2026-04-14 09:51:59 -0400

    Added some stuff

---
+++ Assets/Scripts/UI scripts/Work Type Scripts.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WorkTypeScripts : MonoBehaviour
+{
+    private GameObject player;
+    public int workTime;
+    private work wok;
+
+
+    private void Start()
+    {
+        // 1. Get the root VisualElement from the UIDocument component
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement; // Gets the root element
+
+        // 2. Find the button by name
+        Button myBodyButton = root.Q<Button>("Body"); // Gets the button
+
+        // 3. Assign the function to the 'clicked' event
+        if (myBodyButton != null)
+        {
+            myBodyButton.clicked += OnBodyButtonClick;
+        }
+
+        Button myMindButton = root.Q<Button>("Mind"); // Gets the button
+
+        // 3. Assign the function to the 'clicked' event
+        if (myMindButton != null)
+        {
+            myMindButton.clicked += OnMindButtonClick;
+        }
+
+        Button mySoulButton = root.Q<Button>("Soul"); // Gets the button
+
+        // 3. Assign the function to the 'clicked' event
+        if (mySoulButton != null)
+        {
+            mySoulButton.clicked += OnSoulButtonClick;
+        }
+
+        Button mySpecialButton = root.Q<Button>("Special"); // Gets the button
+
+        // 3. Assign the function to the 'clicked' event
+        if (mySpecialButton != null)
+        {
+            mySpecialButton.clicked += OnSpecialButtonClick;
+        }
+
+        //player=GameObject.Find("Bongbong");
+        
+        //wok=GameObject.Find("Bongbong").GetComponent<work>();
+    }
+
+    private void OnBodyButtonClick()
+    {   
+        Debug.Log("[On Hit] : Murder");
+    }
+    private void OnMindButtonClick()
+    {   
+        Debug.Log("[On Think] : Murder");
+    }
+    private void OnSoulButtonClick()
+    {   
+        Debug.Log("[On Ego] : Murder");
+    }
+    private void OnSpecialButtonClick()
+    {   
+        Debug.Log("[On Special Work] : Murder");
+    }
+    
+}
